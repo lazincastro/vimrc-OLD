359654c48f78e90bcff82487f184dd84f04914c6
Lazaro A. S. Castro <lazarocastro@gmail.com>
2019-04-02 13:45:16 -0300

    Script install

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+#
+# Simple scritp to install and configure vimrc
+# https://github.com/lazarocastro/vimrc
+# By Lazaro Castro
+# 2019-04-02 
+#
+# Tested on Ubuntu 18.04
+
+# Update the system
+sudo apt-get -qq update
+# Install Vim if you don't have.
+sudo apt-get install vim 
+# To work propely, I recomend to you install these programns below:
+# OBS.: The first 6 programs are required, the rest of them are cosmetic issue.
+echo "Install all programns requireds..."
+sleep 5
+sudo apt-get -qq install \
+curl \
+exuberant-ctags \
+git \
+ack-grep \
+ripgrep \
+python-pip \
+tree \
+terminator \
+python-fontforge \
+zsh \
+zsh-syntax-highlighting \
+zsh-theme-powerlevel9k \
+powerline \
+fonts-powerline
+# You'll need of these to work with python scripts/programns.
+sudo pip -q install pep8 flake8 pyflakes isort yapf
+echo "Install Python requireds. Done"
+echo "Download vimrc..."
+sleep 5
+# Download vimrc
+curl https://raw.githubusercontent.com/lazarocastro/vimrc/master/vimrc -o ~/.vimrc
+vim +qa
+clear
+echo "Installation Completed!"
